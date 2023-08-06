448de5e098fe232b811d618ae97bf650d90c4a10
Kaylane Lira <97316221+kaylanelira@users.noreply.github.com>
2023-08-05 22:01:22 -0300

    Comment and organizing

--- cliente.py
+++ cliente.py
@@ -2,15 +2,18 @@ import socket
 import os
 from socket import socket as funcSocket
 
+# configurações da conexão
 buffer_size  = 1024
 
 HOST = 'localhost'
 PORT = 5000
 dest = (HOST, PORT)
 
+# criando socket udp
 udp = funcSocket(socket.AF_INET, socket.SOCK_DGRAM)
 udp.bind(('localhost',3000))
 
+# função que pega o input do user e define o arquivo desejado
 def define_file():
     print("Que tipo de arquivo deseja receber?\n")
     print("(1) PDF\n(2)TXT\n(3) MP4\n(4) MP3\n(5) *IMG\n(6) Desejo encerrar conexao\n")
@@ -28,7 +31,8 @@ def define_file():
         return "testeIMG.jpeg"
     else:
         return "END" 
-    
+
+# função que termina a conexão udp
 def finish_conection():
     finish = "END"
     udp.sendto(finish.encode(),dest)
@@ -36,35 +40,34 @@ def finish_conection():
 
 
 def main(): 
+    # crianção da pasta no cliente
     enderecoChegada = './clienteFile'
 
     if not os.path.exists(enderecoChegada):
         os.makedirs(enderecoChegada)
 
-
-    # enderecoEnvio = str(input())
+    # pegando input do user
     enderecoEnvio = define_file()
 
     while True:
-        #enderecoEnvio = './arquivos/teste.mp4'
 
+        # fim da conexão
         if enderecoEnvio == "END":
             finish_conection()
             break
 
-        # print(enderecoEnvio)
-
+        # enviando arquivo escolhido para o servidor
         with open(enderecoEnvio, 'rb') as f:
             extensao = enderecoEnvio.split('.')[-1] # pegando extensão
             udp.sendto(extensao.encode(), dest) # envio da string codificada
             l = f.read(buffer_size) # lendo o primeiro pacotes de 1024 bytes
             while l:
-                #print(l)
                 udp.sendto(l, dest) # enviando para a porta referenciada
                 l = f.read(buffer_size) # ler os prox 1024 bytes do arq
             udp.sendto(b'', dest) # arquivo vazio para indicar fim
         f.close()
-        
+
+        # recebendo o arquivo que o servidor enviou
         extention, servidor = udp.recvfrom(buffer_size)
 
         with open(f"{enderecoChegada}/arquivoNovo.{extention.decode()}", 'wb') as file: 
@@ -72,7 +75,6 @@ def main():
                 msg, servidor = udp.recvfrom(buffer_size)
                 if not msg:
                     break
-                # print(msg)
                 file.write(msg)
                 file.flush()
         file.close()
@@ -81,4 +83,3 @@ def main():
         
 if __name__ == "__main__":
     main()  
-# udp.sendto(enderecoEnvio.encode(), dest)
