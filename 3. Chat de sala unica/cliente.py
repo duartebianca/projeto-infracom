8362dd79caf0573a97bfb14cb8758b600d531fd0
arbcaio <chab09@gmail.com>
2023-09-21 18:25:45 -0300

    Chat working, using threads to send and receive on client side!

--- 3. Chat de sala unica/cliente.py
+++ 3. Chat de sala unica/cliente.py
@@ -2,6 +2,8 @@ import socket
 import random
 import os
 from socket import socket as funcSocket
+import threading
+import time 
 
 # criando buffer, porta e host
 BUFFER_SIZE  = 1024
@@ -14,11 +16,11 @@ cliente_udp = funcSocket(socket.AF_INET, socket.SOCK_DGRAM)
 cliente_udp.bind((HOST, 3000))
 
 # tempo do timeout
-timeout = 2
+timeout = 1
 
 # função que pega o input do user e define o arquivo desejado
 def print_commands():
-    os.system('cls')
+    os.system('cls') 
     print("-+>"*12, '\n')
     print("Comandos disponiveis:\n")
     print("login_as <nome_do_usuario>")
@@ -39,36 +41,66 @@ def error_gen():
     else: 
         return 0
 
-def snd_pkt(sender, dest, msg): # remetente (quem envia); destinatario (HOST, PORT) - quem recebe o pacote; mensagem
+def snd_pkt(sender, dest, msg, lock): # remetente (quem envia); destinatario (HOST, PORT) - quem recebe o pacote; mensagem
     global timeout
-    sender.settimeout(timeout)
-    if error_gen() == 0:
-        sender.sendto((msg).encode(), dest)
-    flag = 0
-    while True:
-        while flag == 0:
-            try:
-                rcv_msg, _ = sender.recvfrom(BUFFER_SIZE)
-                flag = 1
-            except socket.timeout:
-                if error_gen() == 0:
-                    sender.sendto((msg).encode(), dest)
-        dec_msg = rcv_msg.decode()
-        if dec_msg == 'ack':
-            sender.settimeout(None)
+    with lock:
+        if error_gen() == 0:
+            sender.sendto((msg).encode(), dest)
+        flag = 0
+        while True:
+            while flag == 0:
+                try:
+                    rcv_msg, _ = sender.recvfrom(BUFFER_SIZE)
+                    flag = 1
+                except socket.timeout:
+                    if error_gen() == 0:
+                        sender.sendto((msg).encode(), dest)
+            dec_msg = rcv_msg.decode()
+            if dec_msg == 'ack':
+                # print('ack')
+                pass
             break
 
-def rcv_pkt(dest): # destinatario (HOST, PORT) - quem recebe o pacote
-    rcv_msg, sender = dest.recvfrom(BUFFER_SIZE)
-    dec_msg =  rcv_msg.decode()
-    dest.sendto(('ack').encode(), sender)
-    return sender, dec_msg
+def thread_rcv(dest, lock): # destinatario (HOST, PORT) - quem recebe o pacote;
+    global timeout
+    last_msg = None
+    count = 0
+    rcv_msg = None
+    while True:
+        if count == 3:
+            time.sleep(1)
+            count = 0
+        with lock:
+            count += 1
+            try:
+                rcv_msg, sender = dest.recvfrom(BUFFER_SIZE)                    
+            except socket.timeout:
+                pass
+        if rcv_msg is not None: 
+            if rcv_msg == last_msg:
+                dest.sendto(('ack').encode(), sender)
+            elif 'ack' not in rcv_msg.decode():
+                print(rcv_msg.decode())
+                # print('ack enviado, para mensagem:', rcv_msg.decode())
+                dest.sendto(('ack').encode(), sender)
+                last_msg = rcv_msg
+            else: 
+                pass
+
+def thread_input(sender, dest, lock):
+    while True:
+        msg = input()
+        snd_pkt(sender, dest, msg, lock)    
 
 def main(): 
+
+    cliente_udp.settimeout(timeout)
+    lock = threading.Lock()
     print_commands()
-    while True:
-        command = input()
-        snd_pkt(cliente_udp, server, command)     
+    thread1 = threading.Thread(target=thread_input, args=(cliente_udp, server, lock))
+    thread2 = threading.Thread(target=thread_rcv, args=(cliente_udp, lock))
+    thread2.start()
+    thread1.start()
 
 if __name__ == "__main__":
     main()  
--- 3. Chat de sala unica/servidor.py
+++ 3. Chat de sala unica/servidor.py
@@ -2,21 +2,30 @@ import socket
 import random
 import os
 from socket import socket as funcSocket
+import datetime
+import time
 
 # criando buffer, porta e host
 BUFFER_SIZE  = 1024
 HOST = 'localhost'
 PORT = 5000
-orig = (HOST, PORT)
+orig = (HOST, 3000)
 
 # criando servidor udp
 servidor_udp = funcSocket(socket.AF_INET, socket.SOCK_DGRAM)
-servidor_udp.bind(orig)
+servidor_udp.bind((HOST, PORT))
+
+# timeout
+timeout = 3
 
 def print_chat():
     os.system('cls')
     print("-+>"*12, '\n')
     print("Chat RDT 3.0\n")
+    print("<-+"*12, '\n')
+
+def getTime():
+    return str(datetime.datetime.now().strftime('%X'))
 
 def error_gen():
     numero_aleatorio = random.random()
@@ -30,34 +39,41 @@ def snd_pkt(sender, dest, msg): # remetente (quem envia); destinatario (HOST, PO
     global timeout
     sender.settimeout(timeout)
     if error_gen() == 0:
+        # print('mensagem enviada:', msg)
         sender.sendto((msg).encode(), dest)
-    flag = 0
     while True:
-        while flag == 0:
-            try:
-                rcv_msg, _ = sender.recvfrom(BUFFER_SIZE)
-                flag = 1
-            except socket.timeout:
-                if error_gen() == 0:
-                    sender.sendto((msg).encode(), dest)
+        try:
+            mensagemRecebida, _ = sender.recvfrom(BUFFER_SIZE)
+            decode = mensagemRecebida.decode()
+            # print(decode, 'msg', ('ack' in decode))
+            if 'ack' in decode:
+                sender.settimeout(None)
+                # print('ack recebido para', decode, 'return')
+                return
+        except socket.timeout:
+            # print('except')
+            if error_gen() == 0:
+                # print('mensagem enviada:', msg)
+                sender.sendto((msg).encode(), dest)     
+
+def rcv_pkt_server(dest): # destinatario (HOST, PORT) - quem recebe o pacote
+    dest.settimeout(None)
+    # print('None')
+    while True:
+        rcv_msg, sender = dest.recvfrom(BUFFER_SIZE)
         dec_msg = rcv_msg.decode()
-        if dec_msg == 'ack':
-            sender.settimeout(None)
-            break
-
-def rcv_pkt(dest): # destinatario (HOST, PORT) - quem recebe o pacote
-    rcv_msg, sender = dest.recvfrom(BUFFER_SIZE)
-    dec_msg =  rcv_msg.decode()
-    dest.sendto(('ack').encode(), sender)
-    return sender, dec_msg
-
+        if 'ack' not in dec_msg:
+            dest.sendto(('ack').encode(), sender)
+            return sender, dec_msg
+        
 
 def main():
     print_chat()
     while True:
-        sender, dec_msg = rcv_pkt(servidor_udp)
-        print(sender, dec_msg)
-
+        sender, dec_msg = rcv_pkt_server(servidor_udp)
+        print(sender, dec_msg, getTime())
+        # função de mandar para todos os conectados
+        snd_pkt(servidor_udp, orig, 'mensagem aleatoria ' + str(int(random.random()*100)))
 
 if __name__ == "__main__":
     main()  
